1cb2ef60ef3911432cf4295106f5f27901c020d0
Alireza Zamani <alirezazamani2922@gmail.com>
2021-11-17 20:41:46 +0330

    feat(API): Add Prisma to the project

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
