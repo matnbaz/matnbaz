16b12f5e22e6ffb6f4b48f6fd3f8e619a22f5f4f
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-17 22:39:52 +0330

    WIP dark mode

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,31 @@
 import Button from '../components/UI/Button';
-
+import { useTheme } from 'next-themes';
+import Card from '../components/UI/Card';
+import { HiStar } from 'react-icons/hi';
 export function Index() {
+  const { theme, setTheme } = useTheme();
   return (
-    <div className="p-5 block space-x-2 space-x-reverse">
-      <Button size="sm">تست</Button>
-      <Button size="md">تست</Button>
-      <Button size="lg" variant="secondary">
-        تست
-      </Button>
-      <Button size="xl" href="/google.com">
-        تست
+    <div className="px-8 py-3 grid grid-cols-3 gap-2">
+      {' '}
+      <Card>
+        <div className="flex space-x-3 space-x-reverse self-end justify-end">
+          <div className="flex flex-col space-y-2 h-full items-end">
+            <h1 className="text-3xl font-semibold">facebook/react</h1>
+            <span className="opacity-70 max-w-sm font-thin" dir="ltr">
+              A declarative, efficient, and flexible JavaScript library for
+              building user interfaces.
+            </span>
+          </div>
+          <img
+            src="https://avatars.githubusercontent.com/u/69631?v=4"
+            className="w-28 h-28 rounded-lg"
+          />
+        </div>
+        <HiStar className="w-4 h-4" />
+      </Card>{' '}
+      <br />
+      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        toggle
       </Button>
     </div>
   );
