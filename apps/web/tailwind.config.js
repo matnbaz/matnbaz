0d1f8fe991f4f4a12487004f371f18c189d3c60f
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-20 00:08:57 +0330

    perf(Web): :zap: Enable Tailwind JIT

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  purge: [],
-  jit: true,
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
