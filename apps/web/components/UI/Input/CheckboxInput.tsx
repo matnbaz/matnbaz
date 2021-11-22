5a17aa0f2e11f84dd75754a36cac49d8b318874a
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-22 22:14:23 +0330

    feat(Web): Checkbox input

---
+++ apps/web/components/UI/Input/CheckboxInput.tsx
@@ -0,0 +1,63 @@
+import classNames from 'classnames';
+import { useEffect, useRef, useState } from 'react';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { IInputProps } from './Input';
+
+interface ICheckboxInputProps extends IInputProps {
+  onClick?: () => void;
+  onChange?: (value) => void;
+}
+
+const CheckboxInput = ({
+  className,
+  checked: checkedProp = false,
+  onClick,
+  onChange,
+  ...props
+}: ICheckboxInputProps) => {
+  const [checked, setChecked] = useState(checkedProp);
+  const splittedClassName = className.split(' ');
+  // the given className might not contain width and height, so we find and store them in this object so we could use it if it's provided or set a default width and height if it's not provided
+  const { width, height } = {
+    width: splittedClassName.find((tClass) => tClass.startsWith('w')),
+    height: splittedClassName.find((tClass) => tClass.startsWith('h')),
+  };
+
+  const [defaultWidth, defaultHeight] = ['w-4', 'h-4'];
+
+  if (checked === undefined || checked === null) return <></>;
+
+  return (
+    <button
+      className={classNames(
+        className,
+        width || defaultWidth,
+        height || defaultHeight,
+        checked ? 'bg-primary-500' : 'bg-white dark:bg-gray-600',
+        'rounded-sm shadow-sm cursor-pointer text-white relative'
+      )}
+      onClick={() => {
+        setChecked((previousChecked) => {
+          // OnChange is called here so it's guaranteed that it will have the latest value
+          onChange?.(!previousChecked);
+          return !previousChecked;
+        });
+
+        onClick?.();
+      }}
+    >
+      <input
+        {...props}
+        onChange={onChange}
+        type="checkbox"
+        checked={checked}
+        className="hidden"
+      />
+      {checked && (
+        <AiOutlineCheck className="w-3/4 h-3/4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+      )}
+    </button>
+  );
+};
+
+export default CheckboxInput;
