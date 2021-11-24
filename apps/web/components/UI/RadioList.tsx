13be7eefa39837a00efa304a1094d9456c08f72a
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-24 19:18:50 +0330

    feat(Web): Add Radio List

---
+++ apps/web/components/UI/RadioList.tsx
@@ -0,0 +1,62 @@
+import { RadioGroup } from '@headlessui/react';
+import classNames from 'classnames';
+import { DOMAttributes, useEffect, useMemo, useState } from 'react';
+import Input from './Input/Input';
+interface IOption {
+  name: string;
+}
+
+type TValue = IOption;
+
+interface IRadioListProps {
+  options: IOption[];
+  value?: TValue;
+  className?: string;
+  dir?: 'ltr' | 'rtl';
+  onChange?: (values: TValue) => void;
+}
+
+const RadioList = ({
+  options,
+  value,
+  className,
+  dir = 'rtl',
+  onChange,
+}: IRadioListProps) => {
+  const [selectedOption, setSelectedOption] = useState<TValue>(value);
+
+  useEffect(() => {
+    setSelectedOption(value);
+  }, [value]);
+
+  useEffect(() => {
+    if (selectedOption) onChange(selectedOption);
+  }, [selectedOption]);
+
+  return (
+    <RadioGroup
+      value={selectedOption}
+      onChange={setSelectedOption}
+      as="div"
+      className={classNames(className, 'flex flex-col space-y-4 pb-2')}
+    >
+      {options.map((option) => (
+        <RadioGroup.Option
+          as="div"
+          value={option}
+          className={classNames(
+            dir === 'ltr' && 'text-right',
+            'text-sm text-secondary inline-flex items-center space-x-2 space-x-reverse'
+          )}
+        >
+          <Input.Radio
+            checked={JSON.stringify(selectedOption) === JSON.stringify(option)}
+          />
+          <span>{option.name}</span>
+        </RadioGroup.Option>
+      ))}
+    </RadioGroup>
+  );
+};
+
+export default RadioList;
