f065392c3e78f25e0aea6b46cb52d75f02334623
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-27 20:40:50 +0330

    feat(Web): Add href prop support to card

--- apps/web/components/UI/Card.tsx
+++ apps/web/components/UI/Card.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
-import { DOMAttributes } from 'react';
-
-interface ICardProps extends DOMAttributes<HTMLDivElement | HTMLButtonElement> {
+import { DOMAttributes, useMemo } from 'react';
+import Link, { LinkProps } from 'next/link';
+interface ICardProps
+  extends DOMAttributes<HTMLDivElement | HTMLButtonElement | LinkProps> {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
   colored?: boolean;
   padded?: boolean;
+  href?: string;
 }
 const Card = ({
   children,
@@ -14,20 +16,28 @@ const Card = ({
   colored = false,
   className,
   onClick,
+  href,
   ...props
 }: ICardProps) => {
+  const cardClasses = useMemo(() => {
+    return classNames(
+      className,
+      colored && 'bg-gray-200 dark:bg-gray-800',
+      padded && 'px-5 py-4',
+      'border dark:border-gray-700 rounded-lg'
+    );
+  }, [className, colored, padded]);
+  if (href)
+    return (
+      <Link href={href}>
+        <button {...props} onClick={onClick} className={cardClasses}>
+          {children}
+        </button>
+      </Link>
+    );
   const Component = onClick ? `button` : `div`;
   return (
-    <Component
-      {...props}
-      onClick={onClick}
-      className={classNames(
-        className,
-        colored && 'bg-gray-200 dark:bg-gray-800',
-        padded && 'px-5 py-4',
-        'border dark:border-gray-700 rounded-lg'
-      )}
-    >
+    <Component {...props} onClick={onClick} className={cardClasses}>
       {children}
     </Component>
   );
