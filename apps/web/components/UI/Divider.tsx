1b2492db459b21e0396c886ed4395e7c04950847
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-28 00:14:22 +0330

    feat(Web): Divider className prop

--- apps/web/components/UI/Divider.tsx
+++ apps/web/components/UI/Divider.tsx
@@ -1,7 +1,16 @@
-const Divider = () => {
+import classNames from 'classnames';
+
+interface IDividerProps {
+  className?: string;
+}
+
+const Divider = ({ className }: IDividerProps) => {
   return (
     <div
-      className="w-full rounded-lg bg-gray-200 dark:bg-gray-700"
+      className={classNames(
+        className,
+        'w-full rounded-lg bg-gray-200 dark:bg-gray-700'
+      )}
       style={{ height: '1px' }}
     />
   );
