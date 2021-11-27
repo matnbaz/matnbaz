733053cb99dea8524b953a0012bd2dddc4c77a05
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-28 00:12:23 +0330

    feat(Web): Client portal component

---
+++ apps/web/components/Feature/ClientOnlyPortal.tsx
@@ -0,0 +1,20 @@
+import { useRef, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface Props {
+  children: React.ReactNode;
+  selector: string;
+}
+
+// This is just an implementation of react portals but in the form of a component
+export default function ClientOnlyPortal({ children, selector }: Props) {
+  const ref = useRef<any>();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.querySelector(selector);
+    setMounted(true);
+  }, [selector]);
+
+  return mounted ? createPortal(children, ref.current) : null;
+}
