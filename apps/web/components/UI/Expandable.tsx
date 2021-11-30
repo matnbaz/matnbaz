69226691621a91b1738bcfe42e421247c79d894a
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-11-30 18:03:21 +0330

    fix(Web): Expandable unit fix

--- apps/web/components/UI/Expandable.tsx
+++ apps/web/components/UI/Expandable.tsx
@@ -28,7 +28,7 @@ const Expandable = ({ children, maxHeight = 2400 }: IExpandableProps) => {
         <>
           <div
             className="absolute w-full bottom-0 bg-gradient-to-b from-transparent to-white dark:to-gray-900 pointer-events-none"
-            style={{ height: Math.floor(maxHeight / 4) }}
+            style={{ height: `${Math.floor(maxHeight / 4)}px` }}
           />
           <Button.Ghost
             className="absolute bottom-5 left-1/2 -translate-x-1/2"
