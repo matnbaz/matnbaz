fc417b90deb5baee0b196e40ac0a3c531f6de0a2
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-12-06 13:30:23 +0330

    feat(Web): Make skeleton loader responsive

--- apps/web/components/Skeleton Loader/RepositoryPreviewSkeletonLoader.tsx
+++ apps/web/components/Skeleton Loader/RepositoryPreviewSkeletonLoader.tsx
@@ -3,30 +3,37 @@ import SkeletonLoaderShape from './SkeletonLoaderShape';
 
 const RepositoryPreviewSkeletonLoader = () => {
   return (
-    // TODO: update this for mobile
     <Card padded>
-      <div className="flex space-x-3 space-x-reverse justify-end">
-        <div className="flex flex-col space-y-3 items-end">
+      <div
+        className="flex flex-col md:flex-row md:space-x-3 space-y-3 md:space-y-0"
+        dir="ltr"
+      >
+        <SkeletonLoaderShape
+          shape="circle"
+          width="64px"
+          height="64px"
+          className="flex-shrink-0"
+        />
+        <div className="flex flex-col space-y-3 items-start w-full">
           <SkeletonLoaderShape
             shape="rectangle"
-            width="150px"
+            width="60%"
             height="10px"
             className="mb-1"
           />
 
-          <SkeletonLoaderShape shape="rectangle" width="300px" height="5px" />
-          <SkeletonLoaderShape shape="rectangle" width="300px" height="5px" />
-          <SkeletonLoaderShape shape="rectangle" width="300px" height="5px" />
-          <SkeletonLoaderShape shape="rectangle" width="225px" height="5px" />
+          <SkeletonLoaderShape shape="rectangle" width="100%" height="5px" />
+          <SkeletonLoaderShape shape="rectangle" width="100%" height="5px" />
+          <SkeletonLoaderShape shape="rectangle" width="100%" height="5px" />
+          <SkeletonLoaderShape shape="rectangle" width="90%" height="5px" />
         </div>
-        <SkeletonLoaderShape shape="circle" width="64px" height="64px" />
       </div>
       <div className="flex justify-between items-center w-full mt-8">
         <div className="flex items-center space-x-2 space-x-reverse">
           <SkeletonLoaderShape shape="rectangle" width="21px" height="5px" />
           <SkeletonLoaderShape shape="circle" width="8px" height="8px" />
         </div>
-        <div className="flex space-x-4 space-x-reverse">
+        <div className="flex flex-col md:flex-row space-y-4 space-x-0 md:space-y-0 md:space-x-4 md:space-x-reverse">
           <div className="flex items-center space-x-2 space-x-reverse">
             <SkeletonLoaderShape shape="rectangle" width="20px" height="5px" />
             <SkeletonLoaderShape shape="circle" width="20px" height="20px" />
