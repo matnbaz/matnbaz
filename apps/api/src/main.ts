7d6acfa0fa22da7325dcb4939b02d3a9af73f0ab
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-09 21:44:02 +0330

    feat(API): Basic dockerizing for API (WIP)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,13 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.API_PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
