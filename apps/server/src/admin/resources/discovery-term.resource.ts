b45bba948dc498e03e6cb10e4a3e2059c729bc83
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-16 14:24:06 +0330

    feat(Server): Add temporary schdule dispatchers in admin panel

--- apps/server/src/admin/resources/discovery-term.resource.ts
+++ apps/server/src/admin/resources/discovery-term.resource.ts
@@ -1,9 +1,55 @@
 import { Resource } from './resource-type';
 
-export const discoveryTermResource: Resource = ({ dmmf, prisma }) => ({
+export const discoveryTermResource: Resource = ({ dmmf, prisma, github }) => ({
   resource: {
     model: dmmf.modelMap.DiscoveryTerm,
     client: prisma,
   },
-  options: {},
+  options: {
+    actions: {
+      'discover-all': {
+        name: 'discover-all',
+        isVisible: true,
+        actionType: 'record',
+        icon: 'Checkmark',
+        component: false,
+        variant: 'success',
+        handler: async (request, response, data) => {
+          try {
+            await github.discoverer.discover();
+          } catch (error) {
+            console.error(error);
+          }
+          return {
+            notice: {
+              message: 'Added to the queue.',
+              type: 'success',
+            },
+          };
+        },
+      },
+
+      'extract-all': {
+        name: 'extract-all',
+        isVisible: true,
+        actionType: 'record',
+        icon: 'Checkmark',
+        component: false,
+        variant: 'success',
+        handler: async (request, response, data) => {
+          try {
+            await github.extractor.extract();
+          } catch (error) {
+            console.error(error);
+          }
+          return {
+            notice: {
+              message: 'Added to the queue.',
+              type: 'success',
+            },
+          };
+        },
+      },
+    },
+  },
 });
--- apps/server/src/admin/resources/resource-type.ts
+++ apps/server/src/admin/resources/resource-type.ts
@@ -1,10 +1,15 @@
 import { DMMFClass } from '@prisma/client/runtime';
 import { ResourceWithOptions } from 'adminjs';
 import { PrismaService } from 'nestjs-prisma';
-
+import { GithubDiscovererScheduler } from '../../github-discoverer/github-discoverer.scheduler';
+import { GithubExtractorScheduler } from '../../github-extractor/github-extractor.scheduler';
 export interface ResourceContext {
   dmmf: DMMFClass;
   prisma: PrismaService;
+  github: {
+    discoverer: GithubDiscovererScheduler;
+    extractor: GithubExtractorScheduler;
+  };
 }
 
 export type Resource = (context: ResourceContext) => ResourceWithOptions;
