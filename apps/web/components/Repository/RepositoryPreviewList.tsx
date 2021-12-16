f605d4b0ef8580920a42f3511f1f2cf7ded0fce6
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-12-17 00:26:46 +0330

    feat(Web): Skeleton loader in fetchMore and further refactor

--- apps/web/components/Repository/RepositoryPreviewList.tsx
+++ apps/web/components/Repository/RepositoryPreviewList.tsx
@@ -4,12 +4,20 @@ import InfiniteScroll from '../Feature/InfiniteScroll';
 import RepositoryPreviewSkeletonLoader from '../Skeleton Loader/RepositoryPreviewSkeletonLoader';
 import RepositoryPreview from './RepositoryPreview';
 
-interface IRepositoryPreviewListProps {
+interface IRepositoryPreviewListPropsWithoutPagination {
+  repositories: GetRepositoriesQuery['repositories']['edges'];
+  loading?: never;
+  networkStatus?: never;
+  called?: never;
+  onLoadMore?: never;
+}
+
+interface IRepositoryPreviewListPropsWithPagination {
+  repositories: GetRepositoriesQuery['repositories']['edges'];
+  onLoadMore?: () => void;
   loading: boolean;
   networkStatus?: number;
   called?: boolean;
-  repositories: GetRepositoriesQuery['repositories']['edges'];
-  onLoadMore?: () => void;
 }
 
 const RepositoryPreviewList = ({
@@ -18,29 +26,17 @@ const RepositoryPreviewList = ({
   called = true,
   repositories,
   onLoadMore,
-}: IRepositoryPreviewListProps) => {
+}:
+  | IRepositoryPreviewListPropsWithPagination
+  | IRepositoryPreviewListPropsWithoutPagination) => {
   const mappedRepositories = useMemo(() => {
-    {
-      // Network status 4 is when refetch gets called and network status 3 is for when fetchMore gets called
-      // In this case we don't want skeleton loaders to appear when the user is trying to load more data
-      // So it checks if it's 4
-    }
-
-    return (loading && networkStatus !== 3) || !called ? (
-      <>
-        {[...Array(6).keys()].map((number) => (
-          <RepositoryPreviewSkeletonLoader key={number} />
-        ))}
-      </>
-    ) : (
-      repositories.map((repository) => (
-        <RepositoryPreview
-          repository={repository.node}
-          key={repository.node.fullName}
-        />
-      ))
-    );
-  }, [repositories, loading]);
+    return repositories?.map((repository) => (
+      <RepositoryPreview
+        repository={repository.node}
+        key={repository.node.id}
+      />
+    ));
+  }, [repositories]);
 
   // If pagination is intended for the list, then infinite scroll wrapper is needed
   return onLoadMore ? (
@@ -48,7 +44,25 @@ const RepositoryPreviewList = ({
       onLoadMore={onLoadMore}
       dataLength={repositories?.length || 0}
     >
-      {mappedRepositories}
+      {
+        // Network status 4 is when refetch gets called and network status 3 is for when fetchMore gets called
+        // In this case we don't want skeleton loaders to appear when the user is trying to load more data
+        // So it checks if it's 4
+      }
+
+      {(loading && networkStatus !== 3) || !called ? (
+        <>
+          {[...Array(6).keys()].map((number) => (
+            <RepositoryPreviewSkeletonLoader key={number} />
+          ))}
+        </>
+      ) : (
+        mappedRepositories
+      )}
+      {networkStatus === 3 &&
+        [...Array(2).keys()].map((number) => (
+          <RepositoryPreviewSkeletonLoader key={number} />
+        ))}
     </InfiniteScroll>
   ) : (
     // But if there is no onLoadMore it means that pagination is not needed
