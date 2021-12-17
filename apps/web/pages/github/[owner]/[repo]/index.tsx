b3f1a34fe5492b83f5f0d4c6a6582985bdd02e20
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-18 00:15:21 +0330

    fix(Web): Decrease max width for repo page

--- apps/web/pages/github/[owner]/[repo]/index.tsx
+++ apps/web/pages/github/[owner]/[repo]/index.tsx
@@ -151,7 +151,7 @@ const RepositoryPage = ({ ownerSlug, repoSlug }: RepositoryPageProps) => {
         </div>
       </div>
       <div className="p-5 sm:p-8">
-        <div className="grid grid-cols-1 gap-6 max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 gap-6 max-w-4xl mx-auto">
           <Card padded border="desktop">
             {repo.readmeHtml ? (
               <Expandable>
