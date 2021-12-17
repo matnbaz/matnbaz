407e93d8809065de37d644979bf386f885aaa1a2
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-12-17 23:59:28 +0330

    feat(Web): OwnerImage withoutLink prop

--- apps/web/components/Owner/OwnerImage.tsx
+++ apps/web/components/Owner/OwnerImage.tsx
@@ -1,28 +1,42 @@
 import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useMemo } from 'react';
 interface IOwnerImageProps {
   owner: { platformId: string; login: string; type?: string };
   className?: string;
   width?: number;
   height?: number;
+  withoutLink?: boolean;
 }
 
-const OwnerImage = ({ owner, width, height, className }: IOwnerImageProps) => {
-  return (
+const OwnerImage = ({
+  owner,
+  width,
+  height,
+  className,
+  withoutLink = true,
+}: IOwnerImageProps) => {
+  const ImageComponent = useMemo(
+    () => (
+      <Image
+        width={width || 100}
+        height={height || 100}
+        src={`https://avatars.githubusercontent.com/u/${owner.platformId}?v=4`}
+        alt={`عکس ${owner.login}`}
+        className={classNames(
+          owner?.type === 'User' ? 'rounded-full' : 'rounded-lg',
+          className
+        )}
+      />
+    ),
+    [owner, width, height, className]
+  );
+  return withoutLink ? (
+    ImageComponent
+  ) : (
     <Link href={`/github/${owner.login}`} passHref>
-      <a>
-        <Image
-          width={width || 100}
-          height={height || 100}
-          src={`https://avatars.githubusercontent.com/u/${owner.platformId}?v=4`}
-          alt={`عکس ${owner.login}`}
-          className={classNames(
-            owner?.type === 'User' ? 'rounded-full' : 'rounded-lg',
-            className
-          )}
-        />
-      </a>
+      <a>{ImageComponent}</a>
     </Link>
   );
 };
