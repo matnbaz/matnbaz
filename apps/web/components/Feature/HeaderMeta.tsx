0710c3a772fdc5733447937946eded1c68541b1c
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-17 12:54:16 +0330

    feat(Web): More and begger OG/twitter meta tags

--- apps/web/components/Feature/HeaderMeta.tsx
+++ apps/web/components/Feature/HeaderMeta.tsx
@@ -25,23 +25,38 @@ const HeaderMeta = ({
   return (
     <>
       <Head>
+        <meta name="og:site_name" content="متن‌باز" />
+        <meta name="og:type" content="object" />
+        <meta name="twitter:site" content="@matnbaz_net" />
+        <meta name="twitter:card" content="summary_large_image" />
+
         {title && (
           <>
             <title>{fullTitle}</title>
             <meta name="og:title" content={fullTitle} />
+            <meta name="twitter:title" content={fullTitle} />
           </>
         )}
         {description && (
           <>
             <meta name="og:description" content={description} />
+            <meta name="twitter:description" content={description} />
             <meta name="description" content={description} />
           </>
         )}
         {withBanner ? (
           <>
             <meta name="og:image" content="https://matnbaz.net/banner.jpg" />
-            <meta property="og:image:width" content="1280" />
-            <meta property="og:image:height" content="640" />
+            <meta
+              name="og:image:alt"
+              content="متن‌باز – تمام پروژه‌های متن‌باز ایرانی در یک جا"
+            />
+            <meta
+              name="twitter:image:src"
+              content="https://matnbaz.net/banner.jpg"
+            />
+            <meta name="og:image:width" content="1280" />
+            <meta name="og:image:height" content="640" />
           </>
         ) : (
           image && <meta name="og:image" content={image} />
