e80e530c33594e3ede88aec6a2897d09437b33d9
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-27 07:40:40 +0330

    Cache layer for metadata

--- apps/server/src/resolvers/metadata/metadata.module.ts
+++ apps/server/src/resolvers/metadata/metadata.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '../../cache/cache.module';
 import { MetadataResolver } from './metadata.resolver';
 
 @Module({
+  imports: [CacheModule],
   providers: [MetadataResolver],
 })
 export class MetadataModule {}
--- apps/server/src/resolvers/metadata/metadata.resolver.ts
+++ apps/server/src/resolvers/metadata/metadata.resolver.ts
@@ -1,33 +1,54 @@
-import { CacheControl } from '@exonest/graphql-cache-control';
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
 import { Int, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Cache } from 'cache-manager';
 import { PrismaService } from 'nestjs-prisma';
 import { Metadata } from '../../models/metadata.model';
 
 @Resolver(() => Metadata)
 export class MetadataResolver {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
+  ) {}
+
+  private CACHE_TTL = 21600; // 6 hours
 
   @Query(() => Metadata)
-  @CacheControl({ maxAge: 180 })
   metadata() {
     return {};
   }
 
   @ResolveField(() => Int)
-  totalReposCount() {
-    return this.prisma.repository.count({
-      where: {
-        blockedAt: null,
+  async totalReposCount() {
+    const reposCount = await this.cacheManager.wrap(
+      'totalReposCount',
+      () => {
+        return this.prisma.repository.count({
+          where: {
+            blockedAt: null,
+          },
+        });
       },
-    });
+      { ttl: this.CACHE_TTL }
+    );
+
+    return reposCount;
   }
 
   @ResolveField(() => Int)
-  totalOwnersCount() {
-    return this.prisma.owner.count({
-      where: {
-        blockedAt: null,
+  async totalOwnersCount() {
+    const ownersCount = await this.cacheManager.wrap(
+      'totalOwnersCount',
+      () => {
+        return this.prisma.owner.count({
+          where: {
+            blockedAt: null,
+          },
+        });
       },
-    });
+      { ttl: this.CACHE_TTL }
+    );
+
+    return ownersCount;
   }
 }
