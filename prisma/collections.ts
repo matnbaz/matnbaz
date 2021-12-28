fd467ffe8ae9241275818f3807910d77236288dd
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-28 20:39:47 +0330

    feat(Server): Add color to collection seeds

--- prisma/collections.ts
+++ prisma/collections.ts
@@ -6,61 +6,73 @@ const data = [
   {
     name: 'TypeScript',
     slug: 'typescript',
+    color: '#3178C6',
     Language: { connect: { slug: 'typescript' } },
   },
   {
     name: 'JavaScript',
     slug: 'javascript',
+    color: '#F7DF1E',
     Language: { connect: { slug: 'javascript' } },
   },
   {
     name: 'PHP',
     slug: 'php',
+    color: '#777BB4',
     Language: { connect: { slug: 'php' } },
   },
   {
     name: 'Python',
     slug: 'python',
+    color: '#3776AB',
     Language: { connect: { slug: 'python' } },
   },
   {
     name: 'Rust',
     slug: 'rust',
+    color: '#000000',
     Language: { connect: { slug: 'rust' } },
   },
   {
     name: 'Go',
     slug: 'go',
+    color: '#00ADD8',
     Language: { connect: { slug: 'go' } },
   },
   {
     name: 'Ruby',
     slug: 'ruby',
+    color: '#CC342D',
     Language: { connect: { slug: 'ruby' } },
   },
   {
     name: 'Java',
     slug: 'java',
+    color: '#007396',
     Language: { connect: { slug: 'java' } },
   },
   {
     name: 'C',
     slug: 'c',
+    color: '#A8B9CC',
     Language: { connect: { slug: 'c' } },
   },
   {
     name: 'C++',
     slug: 'cplusplus',
+    color: '#00599C',
     Language: { connect: { slug: 'cplusplus' } },
   },
   {
     name: 'C#',
     slug: 'csharp',
+    color: '#239120',
     Language: { connect: { slug: 'csharp' } },
   },
   {
     name: 'Laravel',
     slug: 'laravel',
+    color: '#FF2D20',
     Topics: { connect: ['laravel'].map((topic) => ({ name: topic })) },
     terms: ['laravel', 'لاراول'],
     Language: { connect: { slug: 'php' } },
@@ -68,6 +80,7 @@ const data = [
   {
     name: 'Node.js',
     slug: 'nodejs',
+    color: '#339933',
     Topics: {
       connect: ['nodejs', 'node-js', 'node'].map((topic) => ({
         name: topic,
@@ -78,6 +91,7 @@ const data = [
   {
     name: 'Flutter',
     slug: 'flutter',
+    color: '#02569B',
     Topics: {
       connect: ['flutter'].map((topic) => ({
         name: topic,
@@ -88,6 +102,7 @@ const data = [
   {
     name: 'React',
     slug: 'react',
+    color: '#61DAFB',
     Topics: {
       connect: ['react', 'react-js', 'reactjs'].map((topic) => ({
         name: topic,
@@ -105,6 +120,7 @@ const data = [
   {
     name: 'Vue.js',
     slug: 'vuejs',
+    color: '#4FC08D',
     Topics: {
       connect: ['vue', 'vuejs'].map((topic) => ({
         name: topic,
@@ -115,6 +131,7 @@ const data = [
   {
     name: 'Angular',
     slug: 'angular',
+    color: '#DD0031',
     Topics: {
       connect: ['angular', 'angularjs'].map((topic) => ({
         name: topic,
