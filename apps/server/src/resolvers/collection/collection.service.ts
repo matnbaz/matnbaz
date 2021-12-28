44f31c5fd4f8c82553a1c0e3ed2580ac8d5079c0
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-28 17:08:31 +0330

    fix(Server): Make collection service injectable

--- apps/server/src/resolvers/collection/collection.service.ts
+++ apps/server/src/resolvers/collection/collection.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'nestjs-prisma';
 
+@Injectable()
 export class CollectionService {
   constructor(private readonly prisma: PrismaService) {}
 
