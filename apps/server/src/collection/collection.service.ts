c3ed911d1bc452e91d5d364745fee4969a41e22e
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-29 20:08:00 +0330

    fix(Server): stop blocked repos from showing up in collections

--- apps/server/src/collection/collection.service.ts
+++ apps/server/src/collection/collection.service.ts
@@ -57,6 +57,7 @@ export class CollectionService {
         OR: [
           {
             AND: [
+              { blockedAt: null },
               {
                 //// Discovery filters
                 OR: [
