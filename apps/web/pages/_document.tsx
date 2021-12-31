bb65bd634505239efb3b52744fcf8ee08edb03a9
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-31 14:52:58 +0330

    feat(Web): Hotjar

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -38,6 +38,18 @@ class AppDocument extends Document {
                 __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
               }}
             />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:${process.env.NEXT_PUBLIC_HOTJAR_SV}};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+              }}
+            />
           </>
         )}
         <body className="bg-white dark:bg-gray-900 selection:bg-primary-400 selection:text-gray-900">
