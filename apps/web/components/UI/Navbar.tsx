f6c7c86d2ee9d1df8bb65659cd732a83ea263175
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-02 18:59:16 +0330

    fix(Web): navbar z-index

--- apps/web/components/UI/Navbar.tsx
+++ apps/web/components/UI/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = ({ className }: INavbarProps) => {
       className={classNames(
         atFirst &&
           'md:bg-transparent dark:md:bg-transparent md:backdrop-blur-none',
-        'z-20 fixed w-full transition-colors backdrop-blur-sm bg-white/60 dark:bg-gray-900/60',
+        'z-30 fixed w-full transition-colors backdrop-blur-sm bg-white/60 dark:bg-gray-900/60',
         className
       )}
     >
