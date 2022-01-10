36f35829454a8189fc68b4b0480e2bd7cbaa421b
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-10 17:34:28 +0330

    feat(Server): Add octokit throttler to prevent getting rate-limited

--- apps/server/src/octokit/octokit.service.ts
+++ apps/server/src/octokit/octokit.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { throttling } from '@octokit/plugin-throttling';
 import { Octokit } from 'octokit';
 
+const MyOctokit = Octokit.plugin(throttling);
+
 @Injectable()
-export class OctokitService extends Octokit {
+export class OctokitService extends MyOctokit {
   constructor() {
-    super({ auth: process.env.GITHUB_TOKEN });
+    super({
+      auth: process.env.GITHUB_TOKEN,
+      throttle: {
+        onRateLimit: (retryAfter, options, octokit) => {
+          octokit.log.warn(
+            `Request quota exhausted for request ${options.method} ${options.url}`
+          );
+
+          if (options.request.retryCount === 0) {
+            // only retries once
+            octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+            return true;
+          }
+        },
+        onAbuseLimit: (retryAfter, options, octokit) => {
+          // does not retry, only logs a warning
+          octokit.log.warn(
+            `Abuse detected for request ${options.method} ${options.url}`
+          );
+        },
+      },
+    });
   }
 }
