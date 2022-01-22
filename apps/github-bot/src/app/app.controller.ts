9a7efd6aba47e9e48c9860587b9bc0a3e56280be
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-22 13:01:25 +0330

    feat: add trigger route to github bot

--- apps/github-bot/src/app/app.controller.ts
+++ apps/github-bot/src/app/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  async follow() {
+    await this.appService.followOwners();
+  }
 }
