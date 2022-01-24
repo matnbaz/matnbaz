09514c2a016be5f481c3c3f2daa48d29a38b0132
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-24 20:18:13 +0330

    feat(Server): Add author resolver to the post module

--- apps/server/src/post/post.module.ts
+++ apps/server/src/post/post.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MarkdownModule } from '../markdown/markdown.module';
+import { PostAuthorResolver } from './post-author.resolver';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
 
 @Module({
   imports: [MarkdownModule],
-  providers: [PostResolver, PostService],
+  providers: [PostService, PostResolver, PostAuthorResolver],
   exports: [PostService],
 })
 export class PostModule {}
