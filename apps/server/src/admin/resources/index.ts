3c8cb1fcefb0d2b2109f16a89824b379c9646ad1
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-26 07:29:29 +0330

    feat(Server): Add user resource to admin panel

--- apps/server/src/admin/resources/index.ts
+++ apps/server/src/admin/resources/index.ts
@@ -10,6 +10,7 @@ import { repositoryResource } from './repository.resource';
 import { ResourceContext } from './resource-type';
 import { submissionResource } from './submission.resource';
 import { topicResource } from './topic.resource';
+import { userResource } from './user.resource';
 
 const resources = [
   repositoryResource,
@@ -23,6 +24,7 @@ const resources = [
   collectResource,
   repositorySelectionResource,
   postResource,
+  userResource,
 ];
 
 export const getResources = (context: ResourceContext) =>
