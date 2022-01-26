27261e82ac7201e78a0cda9b2d55592a3583296d
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-26 08:30:28 +0330

    refactor(Server): comment out post service

--- apps/server/src/post/post.service.ts
+++ apps/server/src/post/post.service.ts
@@ -11,168 +11,168 @@ export class PostService {
     private readonly markdownService: MarkdownService
   ) {}
 
-  /**
-   * @deprecated
-   */
-  async extractPosts() {
-    const mainTreeResponse = await this.octokit.rest.git.getTree({
-      owner: 'matnbaz',
-      repo: 'blog',
-      tree_sha: 'main',
-    });
+  // /**
+  //  * @deprecated
+  //  */
+  // async extractPosts() {
+  //   const mainTreeResponse = await this.octokit.rest.git.getTree({
+  //     owner: 'matnbaz',
+  //     repo: 'blog',
+  //     tree_sha: 'main',
+  //   });
 
-    const postsTreeObject = mainTreeResponse.data.tree.find(
-      (obj) => obj.path === 'posts' && obj.type === 'tree'
-    );
+  //   const postsTreeObject = mainTreeResponse.data.tree.find(
+  //     (obj) => obj.path === 'posts' && obj.type === 'tree'
+  //   );
 
-    if (!postsTreeObject) throw new Error('No posts tree found.');
+  //   if (!postsTreeObject) throw new Error('No posts tree found.');
 
-    const postsTreeResponse = await this.octokit.rest.git.getTree({
-      owner: 'matnbaz',
-      repo: 'blog',
-      tree_sha: postsTreeObject.sha,
-    });
+  //   const postsTreeResponse = await this.octokit.rest.git.getTree({
+  //     owner: 'matnbaz',
+  //     repo: 'blog',
+  //     tree_sha: postsTreeObject.sha,
+  //   });
 
-    const posts = [];
+  //   const posts = [];
 
-    for (const postObject of postsTreeResponse.data.tree) {
-      posts.push(await this.extractPost(postObject.path));
-    }
+  //   for (const postObject of postsTreeResponse.data.tree) {
+  //     posts.push(await this.extractPost(postObject.path));
+  //   }
 
-    return posts;
-  }
+  //   return posts;
+  // }
 
-  /**
-   * @deprecated
-   */
-  async extractPost(postRepoId: string) {
-    const postPath = `posts/${postRepoId}`;
-    const repositoryReference = this.getPostId(postPath);
+  // /**
+  //  * @deprecated
+  //  */
+  // async extractPost(postRepoId: string) {
+  //   const postPath = `posts/${postRepoId}`;
+  //   const repositoryReference = this.getPostId(postPath);
 
-    const post = await this.prisma.post.findUnique({
-      where: {
-        repositoryReference: postRepoId,
-      },
-    });
+  //   const post = await this.prisma.post.findUnique({
+  //     where: {
+  //       repositoryReference: postRepoId,
+  //     },
+  //   });
 
-    const commitsResponse = await this.octokit.rest.repos.listCommits({
-      owner: 'matnbaz',
-      repo: 'blog',
-      path: `${postPath}/content.md`,
-    });
-    const lastCommitDate = new Date(commitsResponse.data[0].commit.author.date);
+  //   const commitsResponse = await this.octokit.rest.repos.listCommits({
+  //     owner: 'matnbaz',
+  //     repo: 'blog',
+  //     path: `${postPath}/content.md`,
+  //   });
+  //   const lastCommitDate = new Date(commitsResponse.data[0].commit.author.date);
 
-    if (post) {
-      if (post.updatedAt > lastCommitDate) {
-        return post;
-      }
-    }
+  //   if (post) {
+  //     if (post.updatedAt > lastCommitDate) {
+  //       return post;
+  //     }
+  //   }
 
-    const postMarkdownResponse = await this.octokit.rest.repos.getContent({
-      owner: 'matnbaz',
-      repo: 'blog',
-      path: `${postPath}/content.md`,
-    });
+  //   const postMarkdownResponse = await this.octokit.rest.repos.getContent({
+  //     owner: 'matnbaz',
+  //     repo: 'blog',
+  //     path: `${postPath}/content.md`,
+  //   });
 
-    const content = Buffer.from(
-      (postMarkdownResponse.data as any).content,
-      'base64'
-    ).toString();
+  //   const content = Buffer.from(
+  //     (postMarkdownResponse.data as any).content,
+  //     'base64'
+  //   ).toString();
 
-    const parsedContent = await this.markdownService.matter(content);
+  //   const parsedContent = await this.markdownService.matter(content);
 
-    parsedContent.content = await this.markdownService.renderForGithub(
-      content,
-      'matnbaz',
-      'blog',
-      'main',
-      postPath
-    );
+  //   parsedContent.content = await this.markdownService.renderForGithub(
+  //     content,
+  //     'matnbaz',
+  //     'blog',
+  //     'main',
+  //     postPath
+  //   );
 
-    const authors = await this.extractPostAuthors(commitsResponse.data);
-    const fields = {
-      content,
-      contentHtml: parsedContent.content,
-      title: parsedContent.data.title,
-      repositoryReference: repositoryReference,
-      slug: parsedContent.data.slug,
-      image: parsedContent.data.image,
-      PostAuthor: {
-        createMany: {
-          data: Object.values(authors),
-        },
-      },
-    };
-    await this.prisma.post.upsert({
-      where: {
-        repositoryReference,
-      },
-      create: {
-        ...fields,
-        User: { connect: { id: process.env.DEFAULT_ADMIN_ID } },
-        publishedAt: new Date(),
-      },
-      update: {
-        ...fields,
-      },
-    });
+  //   const authors = await this.extractPostAuthors(commitsResponse.data);
+  //   const fields = {
+  //     content,
+  //     contentHtml: parsedContent.content,
+  //     title: parsedContent.data.title,
+  //     repositoryReference: repositoryReference,
+  //     slug: parsedContent.data.slug,
+  //     image: parsedContent.data.image,
+  //     PostAuthor: {
+  //       createMany: {
+  //         data: Object.values(authors),
+  //       },
+  //     },
+  //   };
+  //   await this.prisma.post.upsert({
+  //     where: {
+  //       repositoryReference,
+  //     },
+  //     create: {
+  //       ...fields,
+  //       User: { connect: { id: process.env.DEFAULT_ADMIN_ID } },
+  //       publishedAt: new Date(),
+  //     },
+  //     update: {
+  //       ...fields,
+  //     },
+  //   });
 
-    return { post, authors };
-  }
+  //   return { post, authors };
+  // }
 
-  /**
-   * @deprecated
-   */
-  async extractPostAuthors(
-    postCommits: Awaited<
-      ReturnType<OctokitService['rest']['repos']['listCommits']>
-    >['data']
-  ) {
-    const authors: Record<
-      string,
-      { additions: number; deletions: number; ownerId: string }
-    > = {};
+  // /**
+  //  * @deprecated
+  //  */
+  // async extractPostAuthors(
+  //   postCommits: Awaited<
+  //     ReturnType<OctokitService['rest']['repos']['listCommits']>
+  //   >['data']
+  // ) {
+  //   const authors: Record<
+  //     string,
+  //     { additions: number; deletions: number; ownerId: string }
+  //   > = {};
 
-    for (const commit of postCommits) {
-      const commitResponse = await this.octokit.rest.repos.getCommit({
-        owner: 'matnbaz',
-        repo: 'blog',
-        ref: commit.sha,
-      });
+  //   for (const commit of postCommits) {
+  //     const commitResponse = await this.octokit.rest.repos.getCommit({
+  //       owner: 'matnbaz',
+  //       repo: 'blog',
+  //       ref: commit.sha,
+  //     });
 
-      if (authors[commit.author.id]) {
-        authors[commit.author.id].additions +=
-          commitResponse.data.stats.additions;
-        authors[commit.author.id].deletions +=
-          commitResponse.data.stats.deletions;
-      } else {
-        const owner = await this.prisma.owner.findUnique({
-          where: {
-            platform_platformId: {
-              platform: 'GitHub',
-              platformId: commit.author.id.toString(),
-            },
-          },
-        });
+  //     if (authors[commit.author.id]) {
+  //       authors[commit.author.id].additions +=
+  //         commitResponse.data.stats.additions;
+  //       authors[commit.author.id].deletions +=
+  //         commitResponse.data.stats.deletions;
+  //     } else {
+  //       const owner = await this.prisma.owner.findUnique({
+  //         where: {
+  //           platform_platformId: {
+  //             platform: 'GitHub',
+  //             platformId: commit.author.id.toString(),
+  //           },
+  //         },
+  //       });
 
-        if (owner) {
-          authors[commit.author.id] = {
-            additions: commitResponse.data.stats.additions,
-            deletions: commitResponse.data.stats.deletions,
-            ownerId: owner.id,
-          };
-        }
-      }
-    }
-    return authors;
-  }
+  //       if (owner) {
+  //         authors[commit.author.id] = {
+  //           additions: commitResponse.data.stats.additions,
+  //           deletions: commitResponse.data.stats.deletions,
+  //           ownerId: owner.id,
+  //         };
+  //       }
+  //     }
+  //   }
+  //   return authors;
+  // }
 
-  /**
-   * Returns directory's name
-   * @deprecated
-   */
-  private getPostId(path: string) {
-    const split = path.split('/');
-    return split[split.length - 1];
-  }
+  // /**
+  //  * Returns directory's name
+  //  * @deprecated
+  //  */
+  // private getPostId(path: string) {
+  //   const split = path.split('/');
+  //   return split[split.length - 1];
+  // }
 }
