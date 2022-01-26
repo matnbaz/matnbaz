8b9174b4627e919599718bfe4671a41cb2b7ba01
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-26 07:33:54 +0330

    feat(Server): Publish action for post's resource

--- apps/server/src/admin/resources/post.resource.ts
+++ apps/server/src/admin/resources/post.resource.ts
@@ -1,3 +1,4 @@
+import { NotFoundError, ValidationError } from 'adminjs';
 import { Resource } from './resource-type';
 
 export const postResource: Resource = ({ dmmf, prisma }) => ({
@@ -6,6 +7,50 @@ export const postResource: Resource = ({ dmmf, prisma }) => ({
     client: prisma,
   },
   options: {
-    // TODO: Extraction action
+    actions: {
+      publish: {
+        name: 'publish',
+        isVisible: true,
+        actionType: 'record',
+        icon: 'Checkmark',
+        component: false,
+        variant: 'success',
+        handler: async (request, response, data) => {
+          const { record, resource, currentAdmin, h } = data;
+          if (!request.params.recordId || !record) {
+            throw new NotFoundError(
+              ['You have to pass "recordId" to Unblock Action'].join('\n'),
+              'Action#handler'
+            );
+          }
+          try {
+            await resource.update(request.params.recordId, {
+              publishedAt: new Date(),
+            });
+          } catch (error) {
+            if (error instanceof ValidationError && error.baseError) {
+              return {
+                record: record.toJSON(currentAdmin),
+                notice: {
+                  message: error.baseError.message,
+                  type: 'error',
+                },
+              };
+            }
+            throw error;
+          }
+          return {
+            record: record.toJSON(currentAdmin),
+            redirectUrl: h.resourceUrl({
+              resourceId: resource._decorated?.id() || resource.id(),
+            }),
+            notice: {
+              message: 'Successfully published.',
+              type: 'success',
+            },
+          };
+        },
+      },
+    },
   },
 });
