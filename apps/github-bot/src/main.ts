12951d8eef29ae5bca60edb855013cd0e6dd2b62
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-26 19:26:14 +0330

    refactor(Server): remove default comments on main.ts

--- apps/github-bot/src/main.ts
+++ apps/github-bot/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
