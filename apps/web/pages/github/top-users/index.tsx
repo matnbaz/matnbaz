1a8b20e68e91934fb71d1db8b93cd407532ff051
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-29 18:31:47 +0330

    feat(Web): Change the table's template for top users

--- apps/web/pages/github/top-users/index.tsx
+++ apps/web/pages/github/top-users/index.tsx
@@ -30,104 +30,110 @@ const GithubTopUsersPage: NextPage = () => {
         description="در این صفحه کاربران ایرانی گیت‌هاب بر اساس مشارکت‌های عمومی آن‌ها به مخزن‌های اپن‌سورس لیست شده‌اند."
       />
 
-      <div className="shadow overflow-hidden border-b border-zinc-200 dark:border-zinc-700 sm:rounded-lg">
-        <table
-          dir="ltr"
-          className="min-w-full divide-y divide-zinc-200 dark:divide-zinc-700"
-        >
-          <thead className="bg-zinc-50 dark:bg-zinc-800" dir="rtl">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
-              >
-                رتبه
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
-              >
-                نام کاربر
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
-              >
-                شرکت
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
-              >
-                توییتر
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
-              >
-                مشارکت‌های عمومی
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
-              >
-                دنبال‌کنندگان
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white dark:bg-zinc-900 divide-y divide-zinc-200 dark:divide-zinc-700">
-            <InfiniteScroll
-              dataLength={data?.owners.edges.length || 0}
-              onLoadMore={ownersLoadMoreHandler}
-            >
-              {data?.owners.edges.map(({ node: owner }, index) => (
-                <tr key={owner.login}>
-                  <td
-                    className="px-6 py-4 whitespace-nowrap text-center font-bold"
-                    dir="rtl"
+      <div dir="ltr" className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden shadow-md sm:rounded-lg">
+              <table className="min-w-full">
+                <thead className="bg-gray-50 dark:bg-gray-700">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
+                    >
+                      رتبه
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
+                    >
+                      نام کاربر
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
+                    >
+                      شرکت
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-secondary tracking-wider"
+                    >
+                      توییتر
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
+                    >
+                      مشارکت‌های عمومی
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-secondary tracking-wider"
+                    >
+                      دنبال‌کنندگان
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <InfiniteScroll
+                    dataLength={data?.owners.edges.length || 0}
+                    onLoadMore={ownersLoadMoreHandler}
                   >
-                    {persianNumbers(index + 1)}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <img
-                          className="h-10 w-10 rounded-full"
-                          src={`https://github.com/${owner.login}.png`}
-                          alt={`عکس ${owner.name || owner.login}`}
-                        />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm font-medium">{owner.name}</div>
-                        <div className="text-sm text-secondary">
-                          @{owner.login}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm">{owner.company}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm">{owner.twitterUsername}</div>
-                  </td>
-                  <td
-                    className="px-6 py-4 whitespace-nowrap text-center"
-                    dir="rtl"
-                  >
-                    {persianNumbers(owner.publicContributionsCount)}
-                  </td>
-                  <td
-                    className="px-6 py-4 whitespace-nowrap text-center"
-                    dir="rtl"
-                  >
-                    {persianNumbers(owner.followersCount)} نفر
-                  </td>
-                </tr>
-              ))}
-            </InfiniteScroll>
-          </tbody>
-        </table>
+                    {data?.owners.edges.map(({ node: owner }, index) => (
+                      <tr
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                        key={owner.login}
+                      >
+                        <td className="px-6 py-4 whitespace-nowrap text-center font-bold">
+                          <span dir="rtl">{persianNumbers(index + 1)}</span>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 h-10 w-10">
+                              <img
+                                className="h-10 w-10 rounded-full"
+                                src={`https://github.com/${owner.login}.png`}
+                                alt={`عکس ${owner.name || owner.login}`}
+                              />
+                            </div>
+                            <div className="ml-4">
+                              <div className="text-sm font-medium">
+                                {owner.name}
+                              </div>
+                              <div className="text-sm text-secondary">
+                                @{owner.login}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm truncate">
+                            {owner.company}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm">{owner.twitterUsername}</div>
+                        </td>
+                        <td
+                          className="px-6 py-4 whitespace-nowrap text-center"
+                          dir="rtl"
+                        >
+                          {persianNumbers(owner.publicContributionsCount)}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-center">
+                          <span dir="rtl">
+                            {persianNumbers(owner.followersCount)} نفر
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </InfiniteScroll>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
