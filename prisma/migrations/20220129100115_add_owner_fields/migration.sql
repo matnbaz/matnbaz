c0d89c97df1554b2355d32e80fc58f3b4d2642a3
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-29 13:36:05 +0330

    feat(Server): :card_file_box: owner migration

---
+++ prisma/migrations/20220129100115_add_owner_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Owner" ADD COLUMN     "company" TEXT,
+ADD COLUMN     "contributionsCount" INTEGER,
+ADD COLUMN     "followersCount" INTEGER,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "websiteUrl" TEXT;
