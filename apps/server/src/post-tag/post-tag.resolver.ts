130542272fb3517bb72058b4dc810bf1dea8c5c3
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-29 10:51:29 +0330

    feat(Server): Tag resolver

--- apps/server/src/post-tag/post-tag.resolver.ts
+++ apps/server/src/post-tag/post-tag.resolver.ts
@@ -1,8 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from 'nestjs-prisma';
 import { PostTag } from '../models/post-tag.model';
 
 @Resolver(() => PostTag)
 export class PostTagResolver {
   constructor(private readonly prisma: PrismaService) {}
+
+  @Query(() => PostTag, { nullable: true })
+  tag(@Args('name') name: string) {
+    return this.prisma.postTag.findFirst({
+      where: { name: { mode: 'insensitive', equals: name } },
+    });
+  }
 }
