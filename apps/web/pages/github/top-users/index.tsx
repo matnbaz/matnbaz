b7a475cb5cbccc42e27bf275df1d3ef049249746
Alireza Zamani <alirezazamani2922@gmail.com>
2022-02-06 09:26:39 +0330

    fix(Web): bi-di margin

--- apps/web/pages/github/top-users/index.tsx
+++ apps/web/pages/github/top-users/index.tsx
@@ -106,7 +106,7 @@ const GithubTopUsersPage: NextPage = () => {
                                 </a>
                               </Link>
                             </div>
-                            <div className="ltr:mr-4 rtl:ml-4">
+                            <div className="ml-4">
                               <Link href={`/github/${owner.login}`}>
                                 <a target="_blank">
                                   <div className="text-sm font-medium">
