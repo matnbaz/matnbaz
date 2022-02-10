5b3ebcd760327f6c0a5cb2f851a0d8a8d976db20
Alireza Zamani <alirezazamani2922@gmail.com>
2022-02-10 12:43:36 +0330

    fix(Web): repo preview + skeleton loader bi-di

--- apps/web/components/Repository/RepositoryPreview.tsx
+++ apps/web/components/Repository/RepositoryPreview.tsx
@@ -42,12 +42,10 @@ export interface RepositoryPreviewProps
     } | null;
   };
   sendToPlatform?: boolean;
-  variation?: 'default' | 'summary';
 }
 export const RepositoryPreview = ({
   repository,
   sendToPlatform = false,
-  variation = 'default',
   ...props
 }: RepositoryPreviewProps) => {
   const { t } = useTranslation();
@@ -82,9 +80,16 @@ export const RepositoryPreview = ({
   );
   const { locale } = useRouter();
 
-  const template =
-    variation === 'summary' ? (
-      <div className={classNames('relative h-full')}>
+  return (
+    <Card
+      {...props}
+      href={
+        sendToPlatform
+          ? `https://github.com/${repository.fullName}`
+          : `/github/${repository.fullName}`
+      }
+    >
+      <div className="relative h-full" dir="ltr">
         {/* Platform Logo */}
         {/* <img
           className="w-4 h-4 absolute opacity-10"
@@ -92,73 +97,7 @@ export const RepositoryPreview = ({
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
         /> */}
         {repository.isNew && (
-          <span className="px-2 py-0.5 bg-primary-500/30 backdrop-blur-sm rounded-full absolute right-0 text-sm z-20 ltr:-ml-3 rtl:-mr-3 -mt-6">
-            {t('new')}
-          </span>
-        )}
-        <div
-          className={classNames(
-            'flex flex-col items-center space-y-2',
-            hasStatistics && 'pb-4 md:pb-12'
-          )}
-          dir="ltr"
-        >
-          {repository.owner && (
-            <div>
-              <OwnerImage withoutLink owner={repository.owner} />
-            </div>
-          )}
-          <div className="flex flex-col space-y-2 h-full w-full text-left overflow-hidden">
-            <div className="text-lg text-primary-600 dark:text-primary-400 break-all">
-              {repository.fullName}
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-between items-center flex-row-reverse rtl:flex-row md:absolute bottom-0 w-full mt-2 md:mt-0">
-          {repository.language && (
-            <div className="flex items-center">
-              <>
-                {/* // Dir set to ltr to avoid displaying languages like C# as #C */}
-                <div
-                  className="rounded-full w-2 h-2"
-                  style={{
-                    backgroundColor: repository.language.color?.hexString,
-                  }}
-                />
-                <span
-                  className="ltr:pr-1.5 rtl:pl-1.5 py-1.5 text-sm"
-                  dir="ltr"
-                >
-                  {repository.language.name}
-                </span>
-              </>
-            </div>
-          )}
-
-          <div className="flex flex-col-reverse sm:flex-row-reverse gap-4 mr-auto">
-            {hasStatistics &&
-              statistics.slice(0, 1).map((statistic, statIdx) => (
-                <div
-                  key={statIdx}
-                  className="flex flex-row-reverse rtl:flex-row space-x-1 space-x-reverse text-gray-700 dark:text-gray-400 items-center justify-end text-sm sm:text-base"
-                >
-                  <span>{localize(statistic.value, locale)}</span>
-                  <statistic.icon className="w-4 h-4 md:w-5 md:h-5 m-auto" />
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div className="relative h-full">
-        {/* Platform Logo */}
-        {/* <img
-          className="w-4 h-4 absolute opacity-10"
-          style={{ filter: 'invert(1)' }}
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
-        /> */}
-        {repository.isNew && (
-          <span className="px-2 py-0.5 bg-primary-500/30 backdrop-blur-sm rounded-full absolute right-0 text-sm z-20 ltr:-ml-3 rtl:-mr-3 -mt-6">
+          <span className="px-2 py-0.5 bg-primary-500/30 backdrop-blur-sm rounded-full absolute right-0 text-sm z-20 -ml-3 -mt-6">
             {t('new')}
           </span>
         )}
@@ -167,7 +106,6 @@ export const RepositoryPreview = ({
             'flex flex-col md:flex-row md:space-x-3 items-start space-y-2',
             hasStatistics && 'pb-4 md:pb-12'
           )}
-          dir="ltr"
         >
           {repository.owner && (
             <div>
@@ -187,9 +125,22 @@ export const RepositoryPreview = ({
             </p>
           </div>
         </div>
-        <div className="flex justify-between items-center flex-row-reverse rtl:flex-row md:absolute bottom-0 w-full mt-2 md:mt-0">
+        <div className="flex justify-between items-center md:absolute bottom-0 w-full mt-2 md:mt-0">
+          <div className="flex flex-col sm:flex-row gap-4">
+            {hasStatistics &&
+              statistics.map((statistic, statIdx) => (
+                <div
+                  key={statIdx}
+                  className="flex space-x-1 text-gray-700 dark:text-gray-400 items-center justify-end text-sm sm:text-base"
+                >
+                  <statistic.icon className="w-4 h-4 md:w-5 md:h-5 m-auto" />
+                  <span>{localize(statistic.value, locale)}</span>
+                </div>
+              ))}
+          </div>
+
           {repository.language && (
-            <div className="flex items-center" dir="ltr">
+            <div className="flex items-center">
               <>
                 {/* // Dir set to ltr to avoid displaying languages like C# as #C */}
                 <div
@@ -204,33 +155,8 @@ export const RepositoryPreview = ({
               </>
             </div>
           )}
-
-          <div className="flex flex-col-reverse sm:flex-row-reverse gap-4 mr-auto">
-            {hasStatistics &&
-              statistics.map((statistic, statIdx) => (
-                <div
-                  key={statIdx}
-                  className="flex flex-row-reverse rtl:flex-row space-x-1 space-x-reverse text-gray-700 dark:text-gray-400 items-center justify-end text-sm sm:text-base"
-                >
-                  <span>{localize(statistic.value, locale)}</span>
-                  <statistic.icon className="w-4 h-4 md:w-5 md:h-5 m-auto" />
-                </div>
-              ))}
-          </div>
         </div>
       </div>
-    );
-
-  return (
-    <Card
-      {...props}
-      href={
-        sendToPlatform
-          ? `https://github.com/${repository.fullName}`
-          : `/github/${repository.fullName}`
-      }
-    >
-      {template}
     </Card>
   );
 };
