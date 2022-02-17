ff4306d1564f85fa02b345a4b54100b87c80a3a4
Alireza Zamani <alirezazamani2922@gmail.com>
2022-02-17 19:35:26 +0330

    fix(Web): locale selector adding redundant query
    also change the component to use `classNames`

--- apps/web/components/UI/LocaleSelector.tsx
+++ apps/web/components/UI/LocaleSelector.tsx
@@ -1,5 +1,6 @@
 import { Listbox, Transition } from '@headlessui/react';
 import { setBodyDirection } from '@matnbaz/common';
+import classNames from 'classnames';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 import { Fragment, useMemo } from 'react';
@@ -7,15 +8,16 @@ import { HiCheck } from 'react-icons/hi';
 import { IoLanguage } from 'react-icons/io5';
 
 export const LocaleSelector = () => {
-  const { push, pathname, query, locale, locales } = useRouter();
+  const { push, pathname, locale, locales, asPath } = useRouter();
+
   const { t } = useTranslation();
 
   const changeLanguage = useMemo(
     () => async (locale: string) => {
-      await push({ pathname, query }, { pathname, query }, { locale });
+      await push({ pathname }, asPath, { locale });
       setBodyDirection(locale);
     },
-    [push, pathname, query]
+    [push, pathname, asPath]
   );
 
   return (
@@ -38,26 +40,29 @@ export const LocaleSelector = () => {
               <Listbox.Option
                 key={localeIdx}
                 className={({ active }) =>
-                  `${active && 'bg-gray-200 dark:bg-gray-700'}
-                      select-none relative py-2 ltr:pl-10 ltr:pr-4 rtl:pr-10 rtl:pl-4`
+                  classNames(
+                    active && 'bg-gray-200 dark:bg-gray-700',
+                    'select-none relative py-2 ltr:pl-10 ltr:pr-4 rtl:pr-10 rtl:pl-4'
+                  )
                 }
                 value={_locale}
               >
                 {({ selected, active }) => (
                   <>
                     <span
-                      className={`${
-                        selected ? 'font-medium' : 'font-normal'
-                      } block truncate`}
+                      className={classNames(
+                        selected ? 'font-medium' : 'font-normal',
+                        'block truncate'
+                      )}
                     >
                       {t(`locale.${_locale}`)}
                     </span>
                     {selected ? (
                       <span
-                        className={`${
-                          active ? 'text-primary-600' : 'text-primary-600'
-                        }
-                            absolute inset-y-0 ltr:left-0 rtl:right-0 flex items-center ltr:pl-3 rtl:pr-3`}
+                        className={classNames(
+                          active ? 'text-primary-600' : 'text-primary-600',
+                          `absolute inset-y-0 ltr:left-0 rtl:right-0 flex items-center ltr:pl-3 rtl:pr-3`
+                        )}
                       >
                         <HiCheck className="w-5 h-5" aria-hidden="true" />
                       </span>
