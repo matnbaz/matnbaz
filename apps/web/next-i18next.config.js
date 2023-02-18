8f1d220b1a0d4dffc06a2559d744e4a45fdf2dc2
Alireza Zamani <alirezazamani2922@gmail.com>
2023-02-18 23:40:04 +0330

    web configs

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next-i18next').UserConfig */
 module.exports = {
     i18n: {
-        localePath: path.resolve('./apps/web/public/locales'),
+        localePath: path.resolve('./public/locales'),
         defaultLocale: 'fa',
         locales: ['fa', 'en'],
         localeDetection: false,
--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,1 +1,6 @@
-module.exports = require("config/postcss.config");
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
